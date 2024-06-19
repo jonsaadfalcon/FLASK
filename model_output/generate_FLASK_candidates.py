70117c1daf323d8cbd80ae18ecfad99b3742f401
Jon Saad-Falcon <41205309+jonsaadfalcon@users.noreply.github.com>
2024-06-18 18:50:57 -0700

    update

---
+++ model_output/generate_FLASK_candidates.py
@@ -0,0 +1,126 @@
+
+import torch
+from transformers import AutoTokenizer, DebertaForSequenceClassification 
+from transformers import Trainer, TrainingArguments, AutoModelForSequenceClassification, GenerationConfig
+from datasets import load_dataset, load_from_disk
+import transformers
+from transformers.pipelines.pt_utils import KeyDataset
+
+import pandas as pd
+from itertools import combinations, permutations
+from typing import List
+
+import random
+from datasets import Dataset, DatasetDict
+from datetime import datetime
+import sys
+import os
+import logging
+import random
+random.seed(43) #43
+from tabulate import tabulate
+import openai
+import time
+from tqdm import tqdm
+import anthropic
+from safetensors.torch import load_model, save_model 
+import shutil
+import json
+import subprocess
+
+from datasets import Dataset, concatenate_datasets
+
+#################################################
+
+# Parameters
+
+# Mixture of Agents Models
+models = ["Qwen/Qwen1.5-72B-Chat", "Qwen/Qwen1.5-110B-Chat", "microsoft/WizardLM-2-8x22B",
+          "mistralai/Mixtral-8x22B-Instruct-v0.1", "meta-llama/Llama-3-70b-chat-hf", "databricks/dbrx-instruct"]
+MoA_models = models
+
+#models = ["Qwen/Qwen1.5-72B-Chat"]
+models = ["mistralai/Mixtral-8x22B-Instruct-v0.1", "mistralai/Mixtral-8x22B-Instruct-v0.1_v2"]
+
+
+# Generation Settings
+generation_dict = {
+    "batch_size": 8,
+    "temperatures": [0.7], #0.9 #1.5
+    "candidates_per_temp": [1],
+    "generation_max_length": 512,
+    "dataset_cutoff": 4, #3, None
+    #"top_k": 10,
+    #"top_p": 0.9
+}
+
+#################################################
+
+# Ensembling Parameters
+perform_ensembling = True
+ranker_config = {
+    "ranker_checkpoint": "llm-blender/PairRM",
+
+    "ranker_model": "microsoft/deberta-v3-large",
+    "ranker_max_length": 1024, #512, 1024
+    "ranker_batch_size": 16, #32
+    "source_max_length": 256, # 128, 256
+    "candidate_max_length": 256, # 128, 256
+    "device": "cuda:0"
+}
+
+output_dir = "outputs"
+
+#################################################
+
+if not perform_ensembling:
+    
+    for model_name in models:
+
+        print(f"Generating candidates for model: {model_name}")
+        
+        model_id = model_name.split("/")[1]
+        saved_jsonl_path = f"data/mt_bench/model_answer/{model_id}.jsonl"
+        if not os.path.exists(saved_jsonl_path):
+            candidate_generation_command = f"python inference.py --model-path {model_name} --model-id {model_id} --question-file ../input_data/flask_evaluation_raw.jsonl --answer-file {output_dir} --num-gpus 1"
+            candidate_generation_command += f" --model-type TogetherAI --num-choices {generation_dict['candidates_per_temp'][0]}"
+
+            print("Generation Command: ", candidate_generation_command)
+            print("Generating candidates...")
+            assert False
+            #generation_result = subprocess.run(candidate_generation_command, shell=True, capture_output=True, text=True)
+            with subprocess.Popen(candidate_generation_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True) as process:
+                for line in process.stdout:
+                    print(line, end='')  # Print the output in real-time
+
+        else:
+            print(f"Model {model_name} already has candidates generated. Already saved to: {saved_jsonl_path}")
+
+        ##########################################
+
+        judgement_command = f"python gen_judgment.py --model-list {model_id} --parallel 2 --judge-model gpt-4"
+        #judgement_command = f"python gpt4_eval.py -q {output_error_file} -a {answer_file} -o {output_review_file} -e {new_output_error_file}"
+        print("Generating judgements...")
+        judgement_result = subprocess.run(judgement_command, shell=True, capture_output=True, text=True)
+        #breakpoint()
+        #with subprocess.Popen(judgement_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True) as process:
+        #    for line in process.stdout:
+        #        print(line, end='')  # Print the output in real-time
+
+        print("------------------------------------------------")
+        print(f"Judgement Results for {model_name}:")
+        for line in judgement_result.stdout.split("\n"):
+            print(line)
+        print("------------------------------------------------")
+
+        ##########################################
+
+        show_results_command = f"python show_result.py --model-list {model_id}"
+        print("Showing results...")
+        show_results_result = subprocess.run(show_results_command, shell=True, capture_output=True, text=True)
+
+        print("------------------------------------------------")
+        print(f"MTBench Results for {model_name}:")
+        for line in show_results_result.stdout.split("\n"):
+            print(line)
+        print("------------------------------------------------")
--- model_output/inference.py
+++ model_output/inference.py
@@ -16,7 +16,7 @@ def disable_torch_init():
     setattr(torch.nn.LayerNorm, "reset_parameters", lambda self: None)
 
 
-def run_eval(model_path, model_id, question_file, answer_file, num_gpus):
+def run_eval(model_path, model_id, question_file, answer_file, num_gpus, model_type, num_choices):
     # split question file into num_gpus files
     ques_jsons = []
     with open(os.path.expanduser(question_file), "r") as ques_file:
@@ -26,7 +26,8 @@ def run_eval(model_path, model_id, question_file, answer_file, num_gpus):
     chunk_size = len(ques_jsons) // num_gpus
     ans_handles = []
     for i in range(0, len(ques_jsons), chunk_size):
-        ans_handles.append(get_model_answers.remote(model_path, model_id, ques_jsons[i:i + chunk_size]))
+        ans_handles.append(get_model_answers.remote(model_path, model_id, ques_jsons[i:i + chunk_size],
+                                                    model_type=model_type, num_choices=num_choices))
 
     ans_jsons = []
     for ans_handle in ans_handles:
@@ -39,36 +40,45 @@ def run_eval(model_path, model_id, question_file, answer_file, num_gpus):
 
 @ray.remote(num_gpus=1)
 @torch.inference_mode()
-def get_model_answers(model_path, model_id, question_jsons):
-    disable_torch_init()
-    model_path = os.path.expanduser(model_path)
-    tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast= False)
-    model = AutoModelForCausalLM.from_pretrained(model_path,
-        torch_dtype=torch.float16).cuda()
+def get_model_answers(model_path, model_id, question_jsons, model_type, num_choices):
 
-    ans_jsons = []
-    for i, line in enumerate(tqdm(question_jsons)):
-        ques_json = json.loads(line)
-        idx = ques_json["question_id"]
-        qs = ques_json["text"]
-        print("initial question", qs)
-        conv = get_conversation_template(model_id)
-        conv.append_message(conv.roles[0], qs)
-        conv.append_message(conv.roles[1], None)
-        prompt = conv.get_prompt()
+    if model_type == "local":
 
-        inputs = tokenizer([prompt])
-        output_ids = model.generate(
-            torch.as_tensor(inputs.input_ids).cuda(),
-            do_sample=True,
-            temperature=0.7,
-            max_new_tokens=1024)
-        output_ids = output_ids[0][len(inputs.input_ids[0]) :]
-        outputs = tokenizer.decode(output_ids, skip_special_tokens=True).strip()
-        print("cleaned output",outputs)
-        ans_jsons.append({"question_id": idx,
-                          "text": outputs})
-    return ans_jsons
+        disable_torch_init()
+        model_path = os.path.expanduser(model_path)
+        tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast= False)
+        model = AutoModelForCausalLM.from_pretrained(model_path,
+            torch_dtype=torch.float16).cuda()
+
+        ans_jsons = []
+        for i, line in enumerate(tqdm(question_jsons)):
+            ques_json = json.loads(line)
+            idx = ques_json["question_id"]
+            qs = ques_json["text"]
+            print("initial question", qs)
+            conv = get_conversation_template(model_id)
+            conv.append_message(conv.roles[0], qs)
+            conv.append_message(conv.roles[1], None)
+            prompt = conv.get_prompt()
+
+            inputs = tokenizer([prompt])
+            output_ids = model.generate(
+                torch.as_tensor(inputs.input_ids).cuda(),
+                do_sample=True,
+                temperature=0.7,
+                max_new_tokens=1024)
+            output_ids = output_ids[0][len(inputs.input_ids[0]) :]
+            outputs = tokenizer.decode(output_ids, skip_special_tokens=True).strip()
+            print("cleaned output",outputs)
+            ans_jsons.append({"question_id": idx,
+                             "text": outputs})
+        return ans_jsons
+    
+    elif model_type == "TogetherAI":
+        breakpoint()
+    else:
+        raise ValueError("Invalid model type! Model Type Given: ", model_type)
+
 
 
 if __name__ == "__main__":
@@ -78,7 +88,9 @@ if __name__ == "__main__":
     parser.add_argument("--question-file", type=str, default="../input_data/flask_evaluation_raw.jsonl")
     parser.add_argument("--answer-file", type=str, default="outputs/alpaca_7b.jsonl")
     parser.add_argument("--num-gpus", type=int, default=1)
+    parser.add_argument("--model-type", type=str, default="TogetherAI")
+    parser.add_argument("--num-choices", type=int, default=1)
     args = parser.parse_args()
 
     ray.init()
-    run_eval(args.model_path, args.model_id, args.question_file, args.answer_file, args.num_gpus)
+    run_eval(args.model_path, args.model_id, args.question_file, args.answer_file, args.num_gpus, args.model_type, args.num_choices)
