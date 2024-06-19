0fbb1d7dc1fdc3f3deca46039eef7eb50cd5dca0
Jon Saad-Falcon <41205309+jonsaadfalcon@users.noreply.github.com>
2024-06-18 21:01:18 -0700

    update

--- model_output/generate_FLASK_candidates.py
+++ model_output/generate_FLASK_candidates.py
@@ -106,7 +106,9 @@ if not perform_ensembling:
         new_output_error_file = f"{logs_dir}/{model_id}_error_new.jsonl"
 
         #judgement_command = f"python gen_judgment.py --model-list {model_id} --parallel 2 --judge-model gpt-4"
-        judgement_command = f"python {home_repository}gpt4_eval.py -q '../evaluation_set/flask_evaluation.jsonl' -a {answer_file} -o {output_review_file} -e {output_error_file} -r '{home_repository}/outputs/{reference_model}_review.jsonl'"
+        judgement_command = f"python {home_repository}gpt4_eval.py -q '../evaluation_set/flask_evaluation.jsonl' -a {answer_file} -o {output_review_file} -e {output_error_file}"
+        judgement_command += f"-r '{home_repository}/src/reviewer.jsonl'" #" -r '{home_repository}/outputs/{reference_model}_review.jsonl'"
+        judgement_command += f" -p '{home_repository}/src/prompt.jsonl'"
         
         print("Judgement Command: ", judgement_command)
         print("Generating judgements...")
